c926e116337372bf5679c343491c69ef25de6203
diod <vacuum.diod@gmail.com>
2022-10-03 17:26:33 +0300

    add skills bar

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,13 @@
 const hamburger = document.querySelector('.main__hamburger'),
 	menu = document.querySelector('.menu'),
-	closeElem = document.querySelector('.menu__close');
-container = document.querySelector('.container');
+	closeElem = document.querySelector('.menu__close'),
+	container = document.querySelector('.container');
 // обращаемся к документу, чтобы получить элемент <.класс>
+
+const counters = document.querySelectorAll('.charts__percentage'),
+	// Обращаемся ко всем элементам с классом .charts__bar
+	lines = document.querySelectorAll('.charts__bar span')
+// Обращаемся ко всем < span > внутри классов.charts__bar
 
 
 hamburger.addEventListener('click', () => {
@@ -16,4 +21,8 @@ closeElem.addEventListener('click', () => {
 	menu.classList.remove('active');
 	//убираем у элемента с классом .menu класс .active
 	// container.classList.remove('container-active');
+});
+
+counters.forEach((item, i) => {
+	lines[i].style.width = item.innerHTML;
 });
